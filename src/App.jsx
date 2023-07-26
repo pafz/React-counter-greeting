fc7874f76a8ca3bc12eddf8a55030dfec5f45340
pafz <paferza@gmail.com>
2023-07-26 16:07:46 +0200

    2nd part Euralio ended

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import './App.css'
 import CounterFunctional from './components/CounterFunctional/CounterFunctional'
 import GreetingFunctionalFather from './components/GreetingFunctionalFather/GreetingFunctionalFather'
 
-function App() {
+const person = {
+  name: 'Alfonso'
+};
 
+function App() {
   return (
     <>
 <CounterFunctional initialValue={5} numDecre={3} numIncre={9}/>
-<GreetingFunctionalFather name={'Euralio'}/>
+<GreetingFunctionalFather person={person}/>
 
     </>
   )
--- src/components/GreetingFunctionalFather/GreetingFunctionalFather.jsx
+++ src/components/GreetingFunctionalFather/GreetingFunctionalFather.jsx
@@ -1,19 +1,16 @@
 import { useEffect, useState } from "react";
 
 const GreetingFunctionalFather = (props) => {
-  const [name, setName] = useState(props.name);
+  const [name, setName] = useState(props.person.name);
 
-  const nameUser = () => {
-    setName(props.name); //AQUÍ, CAMBIARLO COMO venga como “props” desde el componente padre
-};
 useEffect(()=>{
   setTimeout(() => {
-    
-    nameUser('Alfonsina')
+    setName('Alfonsina')
   }, 1000);
 },[])
+
   return (
-    <p>{name}</p>
+    <p>Hola {name}</p>
     
   )
 }
